5017f1f0687517c36f6cde22477574c090371f16
Edwin García <spark.com.co@gmail.com>
2019-11-26 18:39:23 -0500

    Reto año bisiesto

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/json', (req, res) => {
   res.json({ hello: 'world' });
 });
 
+app.get('/year', (req, res) => {
+  const { year } = req.query;
+  const isDivisibleIntoFour = year % 4 === 0;
+  const isDivisibleIntoOneHundred = year % 100 === 0;
+  const isDivisibleIntoFourHundred = year % 400 === 0;
+  if (!year) {
+    res.send('You must indicate a year in the url with the key "year"');
+  }
+  if (
+    isDivisibleIntoFour &&
+    (!isDivisibleIntoOneHundred || isDivisibleIntoFourHundred)
+  ) {
+    res.send(`${year} es un año bisiesto`);
+  } else {
+    res.send(`${year} no es un año bisiesto`);
+  }
+});
+
 app.listen(config.port, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${config.port}`);
