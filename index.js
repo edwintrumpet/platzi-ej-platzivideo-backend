5b2e765e8dc9916aaf144a658aeba8a1210fdffc
Edwin García <spark.com.co@gmail.com>
2019-11-27 07:14:06 -0500

    Agregando middleware demanejo de errores

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const { config } = require('./config');
 const moviesApi = require('./routes/movies');
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middlewares/errorHandlers');
 
-app.use(express.json())
+app.use(express.json());
 
 moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   // eslint-disable-next-line no-console
