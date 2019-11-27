a45044cb580d4dca8845fc78ccd23c9f2107cd6e
Edwin García <spark.com.co@gmail.com>
2019-11-26 21:06:45 -0500

    Endpoint GET movies

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
 const express = require('express');
 const app = express();
 const { config } = require('./config');
+const moviesApi = require('./routes/movies');
 
-app.get('/', (req, res) => {
-  res.send('hello world!');
-});
-
-app.get('/json', (req, res) => {
-  res.json({ hello: 'world' });
-});
-
-app.get('/year', (req, res) => {
-  const { year } = req.query;
-  const isDivisibleIntoFour = year % 4 === 0;
-  const isDivisibleIntoOneHundred = year % 100 === 0;
-  const isDivisibleIntoFourHundred = year % 400 === 0;
-  if (!year) {
-    res.send('You must indicate a year in the url with the key "year"');
-  }
-  if (
-    isDivisibleIntoFour &&
-    (!isDivisibleIntoOneHundred || isDivisibleIntoFourHundred)
-  ) {
-    res.send(`${year} es un año bisiesto`);
-  } else {
-    res.send(`${year} no es un año bisiesto`);
-  }
-});
+moviesApi(app);
 
 app.listen(config.port, () => {
   // eslint-disable-next-line no-console
