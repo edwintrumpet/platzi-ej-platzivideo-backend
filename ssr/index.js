d2757b3f5162d80d71ec11c7d3732cd4a05b2254
Edwin García <spark.com.co@gmail.com>
2019-12-01 08:31:49 -0500

    Se crea la estrategia de autenticación basic, rutas de sign-in y sign-up

--- ssr/index.js
+++ ssr/index.js
@@ -1,20 +1,60 @@
 const express = require("express");
 const { config } = require("./config");
+const passport = require("passport");
+const Boom = require("@hapi/boom");
+const cookieParser = require("cookie-parser");
+const axios = require("axios");
 
 const app = express();
 const PORT = config.port;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.post('/auth/sign-in', async (req, res, next) => {})
+// Basic Strategy
+require("./utils/auth/strategies/basic");
 
-app.post('/auth/sign-up', async (req, res, next) => {})
+app.post("/auth/sign-in", async (req, res, next) => {
+  passport.authenticate("basic", (error, data) => {
+    try {
+      if (err || !data) {
+        next(Boom.unauthorized());
+      }
+      req.login(data, { session: false }, async err => {
+        if (err) {
+          next(err);
+        }
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
+        res.status(200).json(user);
+      });
+    } catch (err) {
+      next(err);
+    }
+  })(req, res, next);
+});
 
-app.get('/movies', async (req, res, next) => {})
+app.post("/auth/sign-up", async (req, res, next) => {
+  const { body: user } = req;
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+    res.status(200).json({ message: "user created" });
+  } catch (err) {
+    next(err);
+  }
+});
 
-app.post('/user-movies', async (req, res, next) => {})
+app.get("/movies", async (req, res, next) => {});
 
-app.delete('/user-movies/:userMovieId', async (req, res, next) => {})
+app.post("/user-movies", async (req, res, next) => {});
+
+app.delete("/user-movies/:userMovieId", async (req, res, next) => {});
 
 app.listen(PORT, err => {
   if (err) console.log(err);
---
+++ ssr/utils/auth/strategies/basic.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const { BasicStrategy } = require("passport-http");
+const Boom = require("@hapi/boom");
+const axios = require("axios");
+const { config } = require("../../../config");
+
+passport.use(
+  new BasicStrategy(async (email, password, cb) => {
+    try {
+      const { data, status } = await axios({
+        url: `${config.apiUrl}/api/auth/sign-in`,
+        method: "post",
+        auth: {
+          password,
+          username: email
+        },
+        data: {
+          apiKeyToken: config.apiKeyToken
+        }
+      });
+      if (!data || status !== 200) {
+        return cb(Boom.unauthorized(), false);
+      }
+      return cb(null, data);
+    } catch (err) {
+      cb(err);
+    }
+  })
+);
